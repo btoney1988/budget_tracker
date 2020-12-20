3eddc30c7860cc9f889e7bacf3046d022ec298f2
Brian Toney <btoney1988@gmail.com>
2020-12-19 19:25:52 -0500

    service-worker, server.js, manifest, db.js files complete

---
+++ public/db.js
@@ -0,0 +1,50 @@
+let db;
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = function (event) {
+  event.preventdefault();
+  const db = event.target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
+};
+
+request.onsuccess = function (event) {
+  event.preventdefault();
+  db = event.target.result;
+
+  if (navigator.online) {
+    checkDatabase();
+  }
+};
+
+request.onerror = function (event) {
+  event.preventdefault();
+  console.log("Error: " + event.target.errorCode);
+}
+
+function saveRecord(record) {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+
+  store.add(record);
+}
+
+function checkDatabase() {
+  if (getAll.result.length > 0) {
+    fetch("api/transaction/bulk", {
+      method: "POST",
+      body: JSON.stringify(getAll.result),
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => response.json())
+      .then(() => {
+        const transaction = db.transaction(["pending"], "readwrite");
+        const store = transaction.objectStore("pending");
+        store.clear();
+      })
+  };
+}
+
+window.addEventListener("online", checkDatabase);
