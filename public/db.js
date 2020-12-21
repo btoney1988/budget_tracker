7d5a789853ef00528d37fbf9f6e3711a69c4d446
Brian Toney <btoney1988@gmail.com>
2020-12-21 11:45:28 -0500

    adjustments to code for heroku

--- public/db.js
+++ public/db.js
@@ -1,15 +1,20 @@
+const indexedDB =
+  window.indexedDB ||
+  window.mozIndexedDB ||
+  window.webkitIndexedDB ||
+  window.msIndexedDB ||
+  window.shimIndexedDB;
+
 let db;
 const request = indexedDB.open("budget", 1);
 
-request.onupgradeneeded = function (event) {
-  event.preventdefault();
-  const db = event.target.result;
+request.onupgradeneeded = function ({ target }) {
+  db = target.result;
   db.createObjectStore("pending", { autoIncrement: true });
 };
 
-request.onsuccess = function (event) {
-  event.preventdefault();
-  db = event.target.result;
+request.onsuccess = function ({ target }) {
+  db = target.result;
 
   if (navigator.onLine) {
     checkDatabase();
@@ -17,7 +22,6 @@ request.onsuccess = function (event) {
 };
 
 request.onerror = function (event) {
-  event.preventdefault();
   console.log("Error: " + event.target.errorCode);
 }
 
@@ -29,22 +33,29 @@ function saveRecord(record) {
 }
 
 function checkDatabase() {
-  if (getAll.result.length > 0) {
-    fetch("api/transaction/bulk", {
-      method: "POST",
-      body: JSON.stringify(getAll.result),
-      headers: {
-        Accept: "application/json, text/plain, */*",
-        "Content-Type": "application/json"
-      }
-    })
-      .then(response => response.json())
-      .then(() => {
-        const transaction = db.transaction(["pending"], "readwrite");
-        const store = transaction.objectStore("pending");
-        store.clear();
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
       })
-  };
+        .then(response => response.json())
+        .then(() => {
+          const transaction = db.transaction(["pending"], "readwrite");
+          const store = transaction.objectStore("pending");
+          store.clear();
+        })
+    };
+  }
+
 }
 
 window.addEventListener("online", checkDatabase);
